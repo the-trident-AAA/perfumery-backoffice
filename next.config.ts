f31d63be2d2d363069bb0ad6930f6c8cda12fb38
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-05-04 10:23:57 -0400

    chore(next.config): enabled output standalone

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "2mb",
+    },
+  },
 };
 
 export default nextConfig;
