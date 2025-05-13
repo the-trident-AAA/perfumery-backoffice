44202fff4650dcdca327a5cf2661824cf335ea42
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-05-13 18:57:30 -0400

    fix(use-modal): fixed states update in use-modal hook

--- src/components/modal/hooks/useModal.tsx
+++ src/components/modal/hooks/useModal.tsx
@@ -1,44 +1,39 @@
-import { useState } from "react"
-import { InfoModal } from "../types/modalTypes"
+import { useState } from "react";
+import { InfoModal } from "../types/modalTypes";
 
 export default function useModal() {
-	const [currentModal, setCurrentModal] = useState<InfoModal[]>([])
+  const [currentModal, setCurrentModal] = useState<InfoModal[]>([]);
 
-	const handleOpenModal = (infoModal: InfoModal) => {
-		if (infoModal.onOpen) infoModal.onOpen()
-		setCurrentModal([...currentModal, infoModal])
-	}
+  const handleOpenModal = (infoModal: InfoModal) => {
+    if (infoModal.onOpen) infoModal.onOpen();
+    setCurrentModal((prev) => [...prev, infoModal]);
+  };
 
-	const handleCloseModal = (
-		modalName: string,
-		callOnClose: boolean = true,
-	) => {
-		const modalToClose = currentModal.find(
-			modal => modal.name === modalName,
-		)
-		if (modalToClose) {
-			if (modalToClose.onClose && callOnClose) modalToClose.onClose()
-			setCurrentModal(
-				currentModal.filter(infoModal => infoModal.name !== modalName),
-			)
-		}
-	}
+  const handleCloseModal = (modalName: string, callOnClose: boolean = true) => {
+    const modalToClose = currentModal.find((modal) => modal.name === modalName);
+    if (modalToClose) {
+      if (modalToClose.onClose && callOnClose) modalToClose.onClose();
+      setCurrentModal((prev) =>
+        prev.filter((infoModal) => infoModal.name !== modalName)
+      );
+    }
+  };
 
-	const onOpenChange = (modalName: string) => {
-		handleCloseModal(modalName)
-	}
+  const onOpenChange = (modalName: string) => {
+    handleCloseModal(modalName);
+  };
 
-	const getInfoModal = (modalName: string) =>
-		currentModal.find(infoModal => infoModal.name === modalName)
+  const getInfoModal = (modalName: string) =>
+    currentModal.find((infoModal) => infoModal.name === modalName);
 
-	const isModalOpen = (modalName: string) =>
-		currentModal.some(infoModal => infoModal.name === modalName)
-	return {
-		currentModal,
-		onOpenChange,
-		handleOpenModal,
-		handleCloseModal,
-		isModalOpen,
-		getInfoModal,
-	}
+  const isModalOpen = (modalName: string) =>
+    currentModal.some((infoModal) => infoModal.name === modalName);
+  return {
+    currentModal,
+    onOpenChange,
+    handleOpenModal,
+    handleCloseModal,
+    isModalOpen,
+    getInfoModal,
+  };
 }
