905d9f7e1fadbc4a34a16efbde144c65f7f934bd
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-05-23 11:41:36 -0400

    feat(sign-in): implement sign-in functionality with form validation and UI components

---
+++ src/sections/sign-in/form/hooks/use-sign-in.tsx
@@ -0,0 +1,42 @@
+"use client";
+import { useCallback, useState } from "react";
+import { Credentials } from "../schemas/credentials-schema";
+import { signIn as signInAuth } from "@/auth";
+
+interface Props {
+  onSignInAction: () => void;
+}
+
+export default function useSignIn({ onSignInAction }: Props) {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const signIn = useCallback(
+    async (credentials: Credentials) => {
+      try {
+        setLoading(true);
+        setError(null);
+        const res = await signInAuth("credentials", {
+          email: credentials.firstCredential,
+          password: credentials.password,
+          redirect: false,
+        });
+        console.log(res);
+        if (!res) setError("Las credenciales proporcionadas no son correctas");
+        else {
+          onSignInAction();
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    },
+    [onSignInAction]
+  );
+  return {
+    loading,
+    error,
+    signIn,
+  };
+}
---
+++ src/sections/sign-in/form/sign-in-form-container.tsx
@@ -0,0 +1,59 @@
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import React from "react";
+import { FormProvider, useForm } from "react-hook-form";
+import { Credentials, credentialsSchema } from "./schemas/credentials-schema";
+import { Button } from "@/components/ui/button";
+import SignInForm from "./sign-in-form";
+import useSignIn from "./hooks/use-sign-in";
+import { toast } from "react-toastify";
+import { useRouter } from "next/navigation";
+import { paths } from "@/routes/path";
+
+export default function SignInFormContainer() {
+  const router = useRouter();
+  const { signIn, loading: submitLoading } = useSignIn({
+    onSignInAction: () => {
+      toast.success("Inicio de sessión realizado con éxtio");
+      router.push(paths.perfumes.root);
+    },
+  });
+  const form = useForm<Credentials>({
+    resolver: zodResolver(credentialsSchema),
+    defaultValues: {
+      firstCredential: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(credentials: Credentials) {
+    signIn(credentials);
+  }
+
+  return (
+    <FormProvider {...form}>
+      <div className="flex-grow flex flex-col justify-center max-w-md mx-auto w-full">
+        <h1 className="text-3xl font-bold mb-2">
+          Inicia sesión con su cuenta administrativa
+        </h1>
+        <p className="text-gray-500 mb-8">
+          Ingrese el correo electrónico asociado a su cuenta
+        </p>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="w-full flex flex-col gap-6"
+        >
+          <SignInForm />
+          <Button
+            type="submit"
+            variant={"default"}
+            className="w-full"
+            disabled={submitLoading}
+          >
+            Iniciar Sesión
+          </Button>
+        </form>
+      </div>
+    </FormProvider>
+  );
+}
