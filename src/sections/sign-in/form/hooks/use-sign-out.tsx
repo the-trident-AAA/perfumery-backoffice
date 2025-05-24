c469136cbb45b00fa036d47731861a977ca5abd2
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-05-24 13:17:57 -0400

    feat(auth): add custom hook for sign-out functionality with loading and error handling

---
+++ src/sections/sign-in/form/hooks/use-sign-out.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { useCallback, useState } from "react";
+import { signOut as nextAuthSignOut } from "next-auth/react";
+interface Props {
+  onSignOutAction?: () => void;
+}
+
+export default function useSignOut({ onSignOutAction }: Props) {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const signOut = useCallback(async () => {
+    try {
+      setLoading(true);
+      setError(null);
+      await nextAuthSignOut({ redirect: false });
+      if (onSignOutAction) onSignOutAction();
+    } catch (error) {
+      console.log(error);
+      setError("Ocurrió un error inesperado");
+    } finally {
+      setLoading(false);
+    }
+  }, [onSignOutAction]);
+  return {
+    loading,
+    error,
+    signOut,
+  };
+}
