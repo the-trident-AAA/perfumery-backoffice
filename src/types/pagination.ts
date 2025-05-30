d554c893f855e7e3e9086352b5274b435aac3e76
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-05-29 22:10:40 -0400

    refactor(pagination): format and align type definitions for better readability

--- src/types/pagination.ts
+++ src/types/pagination.ts
@@ -1,26 +1,38 @@
 export type IPaginatedData<T> = {
-    data: T[]
-    totalCount: number
-    page: number
-    pageSize: number
-    hasNext: true
-    hasPrevious: true
-}
+  data: T[];
+  totalCount: number;
+  page: number;
+  pageSize: number;
+  hasNext: true;
+  hasPrevious: true;
+};
 
 export type Pagination = {
-    page: number
-    pageSize: number
-    total: number
-}
+  page: number;
+  pageSize: number;
+  total: number;
+};
 
 export type SearchParamsPagination = {
-    sort?: string | string[]
-    search?: string
-    page?: number
-    perPage?: number
-    currentModal?: string
-    categoryId?: string
-    id?: string
-    status?: string
-    classification?: string
+  sort?: string | string[];
+  search?: string;
+  page?: number;
+  perPage?: number;
+  currentModal?: string;
+  categoryId?: string;
+  id?: string;
+  status?: string;
+  classification?: string;
+};
+
+export interface PaginationMeta {
+  total: number;
+  page: number;
+  limit: number;
+  lastPage: number;
+}
+
+export interface PaginationResponse<T> {
+  paginationMeta: PaginationMeta;
+  data: T[];
 }
