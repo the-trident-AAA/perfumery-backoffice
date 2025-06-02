8d760513bca5ebc32ad1bd7c2c96fc4081a0e2f7
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-02 18:01:49 -0400

    feat(filters): implement PerfumesActiveFilters component for displaying active filters

---
+++ src/sections/perfumes/filters/perfumes-active-filters/perfumes-active-filters.tsx
@@ -0,0 +1,105 @@
+import FilterBadge from "@/components/filters/filter-badge/filter-badge";
+import { Label } from "@/components/ui/label";
+import React from "react";
+import { PerfumesFilters } from "../hooks/use-perfumes-filters";
+
+interface Props {
+  filters: PerfumesFilters;
+  handleChangeFilters: (filters: Partial<PerfumesFilters>) => void;
+}
+
+export default function PerfumesActiveFilters({
+  filters,
+  handleChangeFilters,
+}: Props) {
+  return (
+    <div>
+      <div className="space-y-4">
+        <Label>Filtros Activos</Label>
+        <div className="flex flex-wrap gap-2">
+          {filters.name && (
+            <FilterBadge
+              filterName="Nombre"
+              filterValue={filters.name}
+              handleDeleteFilter={() => {
+                handleChangeFilters({ name: undefined });
+              }}
+            />
+          )}
+          {filters.brandId && (
+            <FilterBadge
+              filterName="Marca"
+              filterValue={filters.brandId}
+              handleDeleteFilter={() => {
+                handleChangeFilters({ brandId: undefined });
+              }}
+            />
+          )}
+          {filters.gender && (
+            <FilterBadge
+              filterName="Género"
+              filterValue={filters.gender}
+              handleDeleteFilter={() => {
+                handleChangeFilters({ gender: undefined });
+              }}
+            />
+          )}
+          {filters.available && (
+            <FilterBadge
+              filterName="Disponible"
+              filterValue={filters.available ? "Activos" : "No Activos"}
+              handleDeleteFilter={() => {
+                handleChangeFilters({ available: undefined });
+              }}
+            />
+          )}
+          {filters.perfumeTypeId && (
+            <FilterBadge
+              filterName="Tipo De Perfume"
+              filterValue={filters.perfumeTypeId}
+              handleDeleteFilter={() => {
+                handleChangeFilters({ perfumeTypeId: undefined });
+              }}
+            />
+          )}
+          {filters.scentsIds.length > 0 && (
+            <FilterBadge
+              filterName="Esencias"
+              filterValue={filters.scentsIds.length.toString()}
+              handleDeleteFilter={() => {
+                handleChangeFilters({ scentsIds: [] });
+              }}
+            />
+          )}
+          {filters.offerId && (
+            <FilterBadge
+              filterName="Oferta"
+              filterValue={filters.offerId}
+              handleDeleteFilter={() => {
+                handleChangeFilters({ offerId: undefined });
+              }}
+            />
+          )}
+          {filters.priceRange[0] > 0 && (
+            <FilterBadge
+              filterName="Oferta"
+              filterValue={filters.priceRange[0].toString()}
+              handleDeleteFilter={() => {
+                handleChangeFilters({ priceRange: [0, 1000] });
+              }}
+            />
+          )}
+          {filters.millilitersRange[0] > 0 && (
+            <FilterBadge
+              filterName="Oferta"
+              filterValue={filters.millilitersRange[0].toString()}
+              handleDeleteFilter={() => {
+                handleChangeFilters({ millilitersRange: [0, 100] });
+              }}
+            />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
