5363b1bcbf17b7979bd04ff1bc9345b3c6afa9ec
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-07-05 13:20:20 -0400

    feat(sidebar-header-content): replace the building icon with the shop icon in the SidebarHeaderContent component

--- src/sections/dashboard-layout/components/app-sidebar/sidebar-header-content/sidebar-header-content.tsx
+++ src/sections/dashboard-layout/components/app-sidebar/sidebar-header-content/sidebar-header-content.tsx
@@ -1,14 +1,13 @@
 "use client";
 import { useSidebar } from "@/components/ui/sidebar";
-import { Building2 } from "lucide-react";
+import { StoreIcon } from "lucide-react";
 import React from "react";
 
 export default function SidebarHeaderContent() {
   const { open } = useSidebar();
   return (
     <div className="flex text-white flex-col gap-4 items-center justify-center">
-      <Building2 className={` ${open ? "size-12" : "size-8"}`} />
-
+      <StoreIcon className={` ${open ? "size-12" : "size-8"}`} />
       {open && <p className="text-lg font-bold">Admin Panel</p>}
     </div>
   );
