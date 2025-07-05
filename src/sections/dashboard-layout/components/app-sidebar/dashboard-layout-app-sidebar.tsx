cf21b05ed03562cb9efee320aa2171f3ffca2a83
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-07-05 12:54:42 -0400

    feat(dashboard-layout-app-sidebar): update the Sidebar to include SidebarHeaderContent and improve the header styling

--- src/sections/dashboard-layout/components/app-sidebar/dashboard-layout-app-sidebar.tsx
+++ src/sections/dashboard-layout/components/app-sidebar/dashboard-layout-app-sidebar.tsx
@@ -9,13 +9,16 @@ import {
 import { groupRoutes } from "@/routes/groups-routes/groups-routes";
 import SidebarGroupNavigation from "@/sections/dashboard-layout/components/app-sidebar/sidebar-group-navigation/sidebar-group-navigation";
 import { NavUserContainer } from "../nav-user/nav-user-container";
+import SidebarHeaderContent from "./sidebar-header-content/sidebar-header-content";
 
 export function DashBoardLayoutAppSidebar({
   ...props
 }: React.ComponentProps<typeof Sidebar>) {
   return (
-    <Sidebar collapsible="icon" {...props}>
-      <SidebarHeader>Logo</SidebarHeader>
+    <Sidebar variant="floating" collapsible="icon" {...props}>
+      <SidebarHeader className="flex items-center p-4 rounded-xl justify-center bg-primary">
+        <SidebarHeaderContent />
+      </SidebarHeader>
       <SidebarContent>
         {groupRoutes.map((groupRoute, index) => (
           <SidebarGroupNavigation key={index} group={groupRoute} />
