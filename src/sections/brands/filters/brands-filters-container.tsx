e573eeb85117c643fc5b2e194bb132a552764daf
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-07-05 22:44:00 -0400

    feat(brands-filters-container): integrate the BrandsActiveFilters component to display active filters and improve filter management

--- src/sections/brands/filters/brands-filters-container.tsx
+++ src/sections/brands/filters/brands-filters-container.tsx
@@ -1,14 +1,29 @@
 import React from "react";
 import useBrandsFilters from "./hooks/use-brands-filters";
 import BrandsFilters from "./brands-filters";
+import BrandsActiveFilters from "./components/brands-active-filters/brands-active-filters";
 
 export default function BrandsFiltersContainer() {
-  const { filters, handleChangeFilters, handleResetFilters } = useBrandsFilters(
-    {}
-  );
+  const {
+    filters,
+    handleChangeFilters,
+    handleResetFilters,
+    getActiveFiltersCount,
+  } = useBrandsFilters({});
   return (
-    <>
-    <BrandsFilters filters={filters} handleChangeFilters={handleChangeFilters} />
-    </>
+    <div className="flex flex-col gap-4">
+      <BrandsFilters
+        filters={filters}
+        handleChangeFilters={handleChangeFilters}
+      />
+      {getActiveFiltersCount() > 0 && (
+        <BrandsActiveFilters
+          filters={filters}
+          handleChangeFilters={handleChangeFilters}
+          getActiveFiltersCount={getActiveFiltersCount}
+          handleResetFilters={handleResetFilters}
+        />
+      )}
+    </div>
   );
 }
