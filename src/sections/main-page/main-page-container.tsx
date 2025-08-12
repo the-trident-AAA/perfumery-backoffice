21b6f0840328f1afc489778870d2b252541538be
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-08-12 13:04:27 -0400

    refactor(main-page-container): adjust styles and colors on the home page container to improve visual consistency

--- src/sections/main-page/main-page-container.tsx
+++ src/sections/main-page/main-page-container.tsx
@@ -1,19 +1,21 @@
+import AppLogo from "@/components/app-logo/app-logo";
 import InformationCard from "@/components/information-card/information-card";
 import { paths } from "@/routes/path";
 
 export default function MainPageContainer() {
   return (
-    <div className="flex-1 container mx-auto flex flex-col min-h-screen items-center justify-center px-4 py-12">
+    <div className="flex-1 container mx-auto flex gap-2 flex-col min-h-screen items-center justify-center px-4 py-12">
+      <AppLogo withText={false} avatarClassName="h-32 w-32" />
       <div className="text-center mb-8">
-        <h1 className="text-4xl font-bold tracking-tight text-slate-900 sm:text-5xl mb-4">
+        <h1 className="text-4xl font-bold tracking-tight text-secondary sm:text-5xl mb-4">
           Bienvenido al Panel de Administración
         </h1>
-        <p className="text-xl text-slate-600 max-w-2xl mx-auto">
+        <p className="text-xl text-secondary max-w-2xl mx-auto">
           Gestione su tienda, productos, pedidos y clientes desde un solo lugar.
         </p>
       </div>
 
-      <div className="w-full max-w-5xl grid grid-cols-1 md:grid-cols-2 gap-8 mt-8">
+      <div className="w-full max-w-5xl grid grid-cols-1 md:grid-cols-2 gap-8">
         <InformationCard
           title="Comenzar a Administrar"
           description="Acceda al panel de control para gestionar todos los aspectos de su tienda."
