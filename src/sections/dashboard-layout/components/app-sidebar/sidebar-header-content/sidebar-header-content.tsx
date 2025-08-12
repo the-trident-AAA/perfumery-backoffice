dfad46460cdd6cecca8cc7963e2e767c10bc9a81
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-08-12 10:55:27 -0400

    feat(sidebar-header): adjust avatar size in AppLogo based on sidebar state

--- src/sections/dashboard-layout/components/app-sidebar/sidebar-header-content/sidebar-header-content.tsx
+++ src/sections/dashboard-layout/components/app-sidebar/sidebar-header-content/sidebar-header-content.tsx
@@ -7,7 +7,10 @@ export default function SidebarHeaderContent() {
   const { open } = useSidebar();
   return (
     <div className="flex text-white flex-col gap-4 items-center justify-center">
-      <AppLogo withText={open} />
+      <AppLogo
+        withText={open}
+        avatarClassName={open ? "h-26 w-26" : "h-10 w-10"}
+      />
     </div>
   );
 }
