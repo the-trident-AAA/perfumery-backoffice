d5a28660482e211fd69aee9777b4915159c3767f
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-08-12 10:50:55 -0400

    feat(sidebar): replace store icon with AppLogo component in sidebar header

--- src/sections/dashboard-layout/components/app-sidebar/sidebar-header-content/sidebar-header-content.tsx
+++ src/sections/dashboard-layout/components/app-sidebar/sidebar-header-content/sidebar-header-content.tsx
@@ -1,14 +1,13 @@
 "use client";
+import AppLogo from "@/components/app-logo/app-logo";
 import { useSidebar } from "@/components/ui/sidebar";
-import { StoreIcon } from "lucide-react";
 import React from "react";
 
 export default function SidebarHeaderContent() {
   const { open } = useSidebar();
   return (
     <div className="flex text-white flex-col gap-4 items-center justify-center">
-      <StoreIcon className={` ${open ? "size-12" : "size-8"}`} />
-      {open && <p className="text-lg font-bold">Panel de Administración</p>}
+      <AppLogo withText={open} />
     </div>
   );
 }
