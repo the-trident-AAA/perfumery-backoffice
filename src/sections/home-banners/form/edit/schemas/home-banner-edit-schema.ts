d9d74c6bae5a795c2a6ef292051d5403c03ca280
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-08-22 17:14:31 -0400

    feat(home-banners): update the banner editing schema to include validations for 'statisticalTips' and 'infoTips'

--- src/sections/home-banners/form/edit/schemas/home-banner-edit-schema.ts
+++ src/sections/home-banners/form/edit/schemas/home-banner-edit-schema.ts
@@ -3,6 +3,12 @@ import { z } from "zod";
 export interface HomeBannerEdit {
   title: string;
   description: string;
+  statisticalTips: {
+    statistics: string;
+    info: string;
+  }[];
+
+  infoTips: { name: string }[];
   images: File[];
 }
 
@@ -13,18 +19,35 @@ export const homeBannerEditSchema = z.object({
   description: z.string().min(1, {
     message: "La descripción del banner de la página principal es requerida",
   }),
-  images: z.array(
-    z
-      .instanceof(File, {
-        message: "Por favor selecciona una imagen.",
-      })
-      .refine(
-        (file) => file && file.size <= 5 * 1024 * 1024,
-        "La imagen no debe exceder 5MB."
-      )
-      .refine(
-        (file) => file && file.type.startsWith("image/"),
-        "El archivo debe ser una imagen."
-      )
+  statisticalTips: z.array(
+    z.object({
+      statistics: z
+        .string()
+        .min(1, { message: "Es necesario especificar la estadística" }),
+      info: z.string().min(1, {
+        message: "Es necesario especificar la información de la estadística",
+      }),
+    })
   ),
+  infoTips: z.array(
+    z.object({
+      name: z.string().min(1, { message: "El campo es requerido" }),
+    })
+  ),
+  images: z
+    .array(
+      z
+        .instanceof(File, {
+          message: "Por favor selecciona una imagen.",
+        })
+        .refine(
+          (file) => file && file.size <= 5 * 1024 * 1024,
+          "La imagen no debe exceder 5MB."
+        )
+        .refine(
+          (file) => file && file.type.startsWith("image/"),
+          "El archivo debe ser una imagen."
+        )
+    )
+    .min(1, { message: "Es necesario seleccionar al menos una imagen" }),
 });
