3ed6e37d4ec0ef7cfe8a68983ead8d5533f38207
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-08-21 22:40:01 -0400

    refactor(perfume-details-header): replace image component with ThumbnailsImage to display multiple perfume images

--- src/sections/perfumes/details/perfume-details-header/perfume-details-header.tsx
+++ src/sections/perfumes/details/perfume-details-header/perfume-details-header.tsx
@@ -1,8 +1,8 @@
+import ThumbnailsImage from "@/components/thumbnails-image/thumbnails-image";
 import { Badge } from "@/components/ui/badge";
 import { Separator } from "@/components/ui/separator";
 import { fCurrency } from "@/lib/format-number";
 import { PerfumeDetails } from "@/types/perfumes";
-import Image from "next/image";
 import React from "react";
 
 interface Props {
@@ -13,21 +13,16 @@ export default function PerfumeDetailsHeader({ perfume }: Props) {
   return (
     <div className="flex flex-col md:flex-row gap-6">
       {/* Image */}
-      <div className="w-full md:w-1/3 flex-shrink-0 flex items-center justify-center">
-        <div className="relative aspect-square rounded-md overflow-hidden border w-80 h-80 lg:w-full  lg:h-full border-gray-200">
-          <Image
-            src={perfume.image || "/images/place-holder.jpg"}
-            alt={perfume.name}
-            width={1920}
-            height={1080}
-            className="object-cover w-full h-full"
-          />
-        </div>
-      </div>
+      <ThumbnailsImage
+        images={[perfume.image, ...perfume.images]}
+        altName={perfume.name}
+      />
 
       {/* Header with name and ID */}
       <div className="flex-1">
-        <h2 className="text-2xl font-semibold text-secondary">{perfume.name}</h2>
+        <h2 className="text-2xl font-semibold text-secondary">
+          {perfume.name}
+        </h2>
         <p className="text-sm text-secondary">ID: {perfume.id}</p>
 
         <div className="mt-4 flex flex-col gap-2">
