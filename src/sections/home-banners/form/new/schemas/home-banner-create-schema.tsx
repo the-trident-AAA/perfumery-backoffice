636c1845b624496f05e468b0087a2177d545a465
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-08-22 17:13:25 -0400

    feat(home-banners): add validation to ensure at least one image is selected in the banner creation form

--- src/sections/home-banners/form/new/schemas/home-banner-create-schema.tsx
+++ src/sections/home-banners/form/new/schemas/home-banner-create-schema.tsx
@@ -34,18 +34,20 @@ export const homeBannerCreateSchema = z.object({
       name: z.string().min(1, { message: "El campo es requerido" }),
     })
   ),
-  images: z.array(
-    z
-      .instanceof(File, {
-        message: "Por favor selecciona una imagen.",
-      })
-      .refine(
-        (file) => file && file.size <= 5 * 1024 * 1024,
-        "La imagen no debe exceder 5MB."
-      )
-      .refine(
-        (file) => file && file.type.startsWith("image/"),
-        "El archivo debe ser una imagen."
-      )
-  ),
+  images: z
+    .array(
+      z
+        .instanceof(File, {
+          message: "Por favor selecciona una imagen.",
+        })
+        .refine(
+          (file) => file && file.size <= 5 * 1024 * 1024,
+          "La imagen no debe exceder 5MB."
+        )
+        .refine(
+          (file) => file && file.type.startsWith("image/"),
+          "El archivo debe ser una imagen."
+        )
+    )
+    .min(1, { message: "Es necesario seleccionar al menos una imagen" }),
 });
