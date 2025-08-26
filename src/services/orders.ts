134a5bb0a802f62ce369edb47413db2c4d177ced
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-08-26 15:20:24 -0400

    feat(orders): add order editing functionality, allowing existing order information to be updated via a PATCH request.

--- src/services/orders.ts
+++ src/services/orders.ts
@@ -2,10 +2,9 @@
 import { buildApiResponse } from "@/lib/api";
 import { QueryParamsURLFactory } from "@/lib/request";
 import { apiRoutes, tagsCacheByRoutes } from "@/routes/api-routes/api-routes";
-import { Order, OrderDetails } from "@/types/orders";
+import { Order, OrderDetails, OrderEditDTO } from "@/types/orders";
 import { PaginationResponse } from "@/types/pagination";
 import { IQueryable } from "@/types/request";
-
 
 export async function getOrdersList(params: IQueryable) {
   const url = new QueryParamsURLFactory(params, apiRoutes.orders.get).build();
@@ -22,6 +21,19 @@ export async function getOrderById(id: string) {
   const res = await fetch(apiRoutes.orders.getById.replace(":id", id), {
     method: "GET",
     next: { tags: [tagsCacheByRoutes.orders.singleTag] },
+  });
+
+  return await buildApiResponse<OrderDetails>(res);
+}
+
+export async function editOrder(id: string, orderEditDTO: OrderEditDTO) {
+  const res = await fetch(apiRoutes.orders.getById.replace(":id", id), {
+    method: "PATCH",
+    headers: {
+      Authorization: "Bearer " + "token",
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(orderEditDTO),
   });
 
   return await buildApiResponse<OrderDetails>(res);
