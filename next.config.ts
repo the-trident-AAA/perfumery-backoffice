ede53bbcfe3805ae70177a29ae4617294b6f6831
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-09-25 16:52:49 -0400

    refactor(next.config): simplify serverActions configuration and remove unnecessary properties in next.config.ts.

--- next.config.ts
+++ next.config.ts
@@ -3,18 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
-  experimental: {
-    serverActions: {
-      bodySizeLimit: "2mb",
-    },
+  serverActions: {
+    bodySizeLimit: "2mb",
   },
   images: {
     remotePatterns: [
       {
         protocol: "http",
         hostname: process.env.NEXT_PUBLIC_IMAGE_HOST || "localhost",
-        port: process.env.NEXT_PUBLIC_IMAGE_PORT || "9000",
-        pathname: "/perfumery/**",
       },
     ],
   },
