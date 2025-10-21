be3b36f206954d6ae9a77a9f77bf0c4260eb3caf
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-10-21 17:23:15 -0400

    feat(edit-order-form): enhance order display with order code, state badge, and last update date

--- src/sections/orders/form/edit/edit-order-form-container.tsx
+++ src/sections/orders/form/edit/edit-order-form-container.tsx
@@ -11,8 +11,15 @@ import FormActionButtons from "@/components/form/components/form-action-buttons/
 import { AlertDestructive } from "@/components/ui/alert-destructive";
 import useEditOrder from "../../hooks/use-edit-order";
 import { OrderEdit, orderEditSchema } from "./schemas/order-edit-schema";
-import { OrderDetails } from "@/types/orders";
+import {
+  getOrderStateText,
+  getOrderStateVariant,
+  OrderDetails,
+} from "@/types/orders";
 import OrderForm from "../order-form";
+import { Box, Package } from "lucide-react";
+import { Badge } from "@/components/ui/badge";
+import { formatDate } from "@/lib/format-date";
 
 interface Props {
   order: OrderDetails;
@@ -59,6 +66,37 @@ export default function EditOrderFormContainer({ order }: Props) {
         onSubmit={form.handleSubmit(onSubmit)}
         className="w-full flex flex-1 flex-col justify-between gap-8 h-full"
       >
+        <div className="flex items-center gap-4">
+          <div className="bg-secondary p-3 rounded-full">
+            <Package className="h-6 w-6 text-primary" />
+          </div>
+
+          <div className="flex flex-col items-start">
+            <span className="font-bold text-xl text-secondary group-hover:text-primary/80 transition-colors">
+              Pedido {order.code}
+            </span>
+
+            <div className="flex flex-wrap items-center gap-4">
+              <Badge
+                className={`${getOrderStateVariant(
+                  order.state
+                )} mt-2 flex items-center gap-1 px-3 py-1`}
+              >
+                <Box className="h-3 w-3" />
+                {getOrderStateText(order.state)}
+              </Badge>
+              <div
+                className="flex flex-col gap-1
+										"
+              >
+                <p className="text-sm text-secondary">Última actualización:</p>
+                <Badge variant={"secondary"} >
+                  {formatDate(order.lastUpdateDate)}
+                </Badge>
+              </div>
+            </div>
+          </div>
+        </div>
         {editBrandError && <AlertDestructive title={editBrandError} />}
         <OrderForm order={order} />
         <FormActionButtons
