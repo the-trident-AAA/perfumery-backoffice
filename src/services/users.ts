7d18cb16efa1fdf5a10dc02545ab6c2bb7b21421
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-10-20 22:24:43 -0400

    feat(users): implement authentication for user list and user details retrieval with error handling for unauthorized access

--- src/services/users.ts
+++ src/services/users.ts
@@ -1,4 +1,5 @@
 "use server";
+import { auth } from "@/auth";
 import { buildApiResponse } from "@/lib/api";
 import { QueryParamsURLFactory } from "@/lib/request";
 import { apiRoutes, tagsCacheByRoutes } from "@/routes/api-routes/api-routes";
@@ -6,21 +7,48 @@ import { IQueryable } from "@/types/request";
 import { User, UserDetails } from "@/types/users";
 
 export async function getUsersList(params: IQueryable) {
-  console.log("entro a getUsersList");
+  const session = await auth();
+  if (!session)
+    return {
+      error: {
+        name: "Unauthorized",
+        reason: "No está autorizado para usar este recurso",
+        code: "401",
+      },
+      status: 401,
+    };
   const url = new QueryParamsURLFactory(params, apiRoutes.users.get).build();
 
   const res = await fetch(url, {
     method: "GET",
+    headers: {
+      Authorization: "Bearer " + session.accessToken,
+      "content-type": "application/json",
+    },
     next: { tags: [tagsCacheByRoutes.users.multipleTag] },
   });
 
-  console.log("antes de retornar buildApiResponse");
   return await buildApiResponse<User[]>(res);
 }
 
 export async function getUserById(id: string) {
+  const session = await auth();
+  if (!session)
+    return {
+      error: {
+        name: "Unauthorized",
+        reason: "No está autorizado para usar este recurso",
+        code: "401",
+      },
+      status: 401,
+    };
+
   const res = await fetch(apiRoutes.users.getById.replace(":id", id), {
     method: "GET",
+    headers: {
+      Authorization: "Bearer " + session.accessToken,
+      "content-type": "application/json",
+    },
     next: { tags: [tagsCacheByRoutes.users.singleTag] },
   });
 
