9bb0fbff3c80419ae6264fe03aca877df6b561e1
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-10-21 22:52:50 -0400

    feat(auth): add TokenPayLoad interface to define structure for authentication tokens

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -1,8 +1,18 @@
 import { Credentials } from "@/sections/sign-in/form/schemas/credentials-schema";
+import { UserRole } from "./users";
 
 export interface CredentialsDTO {
   username: string;
   password: string;
+}
+
+export interface TokenPayLoad {
+  accessToken: string;
+  id: string;
+  username: string;
+  email: string;
+  shopCartId: string;
+  role: UserRole;
 }
 
 export const convertCredentialsDTO = (
