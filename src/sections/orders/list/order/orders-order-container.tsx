3e75bf10103ba5e04a99d6fa77b29fc283471661
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-11-07 16:31:53 -0500

    feat(orders): add OrdersOrderContainer for sorting options in orders list

---
+++ src/sections/orders/list/order/orders-order-container.tsx
@@ -0,0 +1,27 @@
+"use client";
+import SorterComponent from "@/components/sorter-component/sorter-component";
+import useUrlFilters from "@/hooks/use-url-filters";
+
+import React from "react";
+
+export default function OrdersOrderContainer() {
+  const { updateFiltersInUrl } = useUrlFilters();
+  return (
+    <SorterComponent
+      sortOptions={[
+        {
+          key: "lastUpdateDate",
+          label: "Fecha de actualización",
+          type: "date",
+        },
+      ]}
+      defaultSort="lastUpdateDate"
+      onSortChange={(sortKey: string, direction: "asc" | "desc") => {
+        updateFiltersInUrl({
+          orderBy: sortKey,
+          order: direction.toLocaleUpperCase(),
+        });
+      }}
+    />
+  );
+}
