b334693ff7248a5923b5d114edd973438dc6c08d
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-11-12 21:08:22 -0500

    refactor(tapes): enhance TapeForm component to include image upload and text field for improved user input

--- src/sections/tapes/form/tapes-form.tsx
+++ src/sections/tapes/form/tapes-form.tsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import { RHFImageUpload } from "@/components/form/rhf-components/rhf-image-upload/rhf-image-upload";
+import { RHFTextField } from "@/components/form/rhf-components/rhf-text-field/rhf-text-field";
+import { Card, CardContent } from "@/components/ui/card";
+import React from "react";
 
-export default function TapeForm() {
+interface Props {
+  imageRecived?: {
+    loading: boolean;
+    error: string | null;
+  };
+}
+
+export default function TapeForm({ imageRecived }: Props) {
   return (
-    <div>TapeForm</div>
-  )
+    <Card className="shadow-sm bg-muted">
+      <CardContent className="pt-4 px-4 flex flex-col gap-4">
+        <RHFTextField
+          name="name"
+          label="Nombre de la cinta"
+          placeholder="Ingrese el nombre de la cinta"
+        />
+
+        <RHFImageUpload
+          name="image"
+          label="Imagen del Home Banner"
+          {...(imageRecived && { loading: imageRecived.loading })}
+        />
+      </CardContent>
+    </Card>
+  );
 }
