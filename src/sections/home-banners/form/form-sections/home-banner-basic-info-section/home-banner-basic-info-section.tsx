dbfb4dcc150f439baf3644b44408dc9765bc572f
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-11-16 13:00:57 -0500

    feat(home-banners): add RHFSelectField for text color selection in HomeBannerBasicInfoSection

--- src/sections/home-banners/form/form-sections/home-banner-basic-info-section/home-banner-basic-info-section.tsx
+++ src/sections/home-banners/form/form-sections/home-banner-basic-info-section/home-banner-basic-info-section.tsx
@@ -5,6 +5,8 @@ import { RHFTextField } from "@/components/form/rhf-components/rhf-text-field/rh
 import { RHFTextAreaField } from "@/components/form/rhf-components/rhf-text-area-field/rhf-text-area-field";
 import { RHFImageUpload } from "@/components/form/rhf-components/rhf-image-upload/rhf-image-upload";
 import ButtonTextField from "./components/button-text-field";
+import { RHFSelectField } from "@/components/form/rhf-components/rhf-select-field/rhf-select-field";
+import { getTextColorText, TextColor } from "@/types/home-banners";
 
 interface Props {
   imageRecived?: {
@@ -34,6 +36,21 @@ export default function HomeBannerBasicInfoSection({
           label="Descripción del Banner"
           description="Introduzca la descripción del banner"
           fullWidth
+        />
+        <RHFSelectField
+          name="textColor"
+          label="Color de los textos"
+          placeholder="Seleccione un color de texto deseado"
+          options={[
+            {
+              value: TextColor.LIGHT,
+              label: getTextColorText(TextColor.LIGHT),
+            },
+            {
+              value: TextColor.DARK,
+              label: getTextColorText(TextColor.DARK),
+            },
+          ]}
         />
         <ButtonTextField />
         <RHFImageUpload
